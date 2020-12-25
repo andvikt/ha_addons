94975ec6bc39a4689044e9638852e5a099424bf7
andvikt <andvikt@gmail.com>
2020-12-25 20:28:06 +0300

    add devices

--- noolite/build.sh
+++ noolite/build.sh
@@ -1,5 +1,10 @@
 docker run --rm -ti --name hassio-builder --privileged \
-  -v /home/andvikt/projects/noolite:/data \
+  -v /home/andvikt/projects/ha_addons/noolite:/data \
   -v /var/run/docker.sock:/var/run/docker.sock:ro \
   homeassistant/amd64-builder -t /data --aarch64 --test \
-  -i noolite_addon -d local
+  -i noolite \
+  --docker-hub andvikt \
+  --self-cache \
+  --no-crossbuild-cleanup \
+  --docker-user andvikt \
+  --docker-password $DOCKER_PWD
